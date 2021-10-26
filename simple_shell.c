032a9f3d604cb5446751a0c6bcd06a3d1bcfc57c
drwamunga <87418352+drwamunga@users.noreply.github.com>
2021-10-26 12:09:02 -0700

    Command_line

---
+++ simple_shell.c
@@ -0,0 +1,21 @@
+#include "header.h"
+/**
+ *main - main simple shell function
+ *@ac : argument count
+ *@av : argument vector
+ * envp : environment variables
+ * Return : int
+ */
+
+ int main(int ac, char **av, char **envp)
+{
+  {
+	char *lineptr = NULL, **cmds, *fp_res;
+	size_t s = 0;
+	int status = 0, cmd_n = 0;
+	ssize_t n_chars;
+
+	(void)ac;
+	(void)envp;
+  return (0);
+}
